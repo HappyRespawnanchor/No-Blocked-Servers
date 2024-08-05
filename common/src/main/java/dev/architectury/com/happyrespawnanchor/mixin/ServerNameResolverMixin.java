03fb860c677a7304c7e1d9704b05abdaa12e39b3
HappyRespawnanchor <happyrespawnanchor@gmail.com>
2024-08-05 18:29:09 +0800

    First Commit

---
+++ common/src/main/java/dev/architectury/com/happyrespawnanchor/mixin/ServerNameResolverMixin.java
@@ -0,0 +1,22 @@
+package dev.architectury.com.happyrespawnanchor.mixin;
+
+import net.minecraft.client.multiplayer.resolver.AddressCheck;
+import net.minecraft.client.multiplayer.resolver.ResolvedServerAddress;
+import net.minecraft.client.multiplayer.resolver.ServerAddress;
+import net.minecraft.client.multiplayer.resolver.ServerNameResolver;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(ServerNameResolver.class)
+public abstract class ServerNameResolverMixin {
+    @Redirect(method = "resolveAddress", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/resolver/AddressCheck;isAllowed(Lnet/minecraft/client/multiplayer/resolver/ServerAddress;)Z"))
+    private boolean redirectIsAllowed(AddressCheck addressCheck, ServerAddress address) {
+        return true;
+    }
+    @Redirect(method = "resolveAddress", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/resolver/AddressCheck;isAllowed(Lnet/minecraft/client/multiplayer/resolver/ResolvedServerAddress;)Z"))
+    private boolean redirectIsAllowed(AddressCheck addressCheck, ResolvedServerAddress address) {
+        return true;
+    }
+
+}
