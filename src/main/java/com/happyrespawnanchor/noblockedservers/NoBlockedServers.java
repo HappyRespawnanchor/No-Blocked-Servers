333330053c57fe28b0950790af759eaa57e8443b
HappyRespawnanchor <80967824+HappyRespawnanchor@users.noreply.github.com>
2024-11-01 20:31:41 +0800

    init forge

---
+++ src/main/java/com/happyrespawnanchor/noblockedservers/NoBlockedServers.java
@@ -0,0 +1,10 @@
+package com.happyrespawnanchor.noblockedservers;
+
+import net.minecraftforge.fml.common.Mod;
+
+import static com.happyrespawnanchor.noblockedservers.NoBlockedServers.MOD_ID;
+
+@Mod(MOD_ID)
+public class NoBlockedServers {
+    public static final String MOD_ID = "no_blocked_servers";
+}
